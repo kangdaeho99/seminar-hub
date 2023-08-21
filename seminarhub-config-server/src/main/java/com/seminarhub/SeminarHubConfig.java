4e21f4225a4745347001bac7207f3f0b3ac688db
daeho kang <eoghdhdls@naver.com>
2023-08-21 19:48:13 +0900

    feat: spring cloud config Eureka에 등록

--- seminarhub-config-server/src/main/java/com/seminarhub/SeminarHubConfig.java
+++ seminarhub-config-server/src/main/java/com/seminarhub/SeminarHubConfig.java
@@ -2,6 +2,7 @@ package com.seminarhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class SeminarHubConfig {
     public static void main(String[] args) {
         System.out.println("Hello world!");
