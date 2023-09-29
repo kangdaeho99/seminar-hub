ad739eac430a76fe2888197b82f66e1f7e2648e0
daeho kang <eoghdhdls@naver.com>
2023-09-29 16:02:21 +0900

    feat:  		implementation group: 'org.springframework.cloud', name: 'spring-cloud-starter-kubernetes', version: '1.1.10.RELEASE', org.springdoc:springdoc-openapi-webflux-ui:1.4.3추가, '/serivce', '/instance' rest controller 추가

--- seminarhub-cloud-gateway-server/src/main/java/com/seminarhub/SeminarHubCloudGateway.java
+++ seminarhub-cloud-gateway-server/src/main/java/com/seminarhub/SeminarHubCloudGateway.java
@@ -1,8 +1,17 @@
 package com.seminarhub;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * [ 2023-08-18 daeho.kang ]
@@ -11,9 +20,29 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class SeminarHubCloudGateway {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         SpringApplication.run(SeminarHubCloudGateway.class, args);
     }
+
+    @GetMapping("/services")
+    public List<String> services(){
+        return this.discoveryClient.getServices();
+    }
+    @GetMapping("/instance")
+    public List<List<ServiceInstance>> instance(){
+        ArrayList<List<ServiceInstance>> arr = new ArrayList<>();
+        for(int i=0;i<discoveryClient.getServices().size();i++){
+            arr.add(discoveryClient.getInstances(discoveryClient.getServices().get(i)));
+        }
+        return arr;
+    }
+
+
 }
