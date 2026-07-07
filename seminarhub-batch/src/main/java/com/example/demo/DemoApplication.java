c88f946f692de9f0027f6e2461323154441e1780
kangdaeho99 <kangdaeho99>
2026-07-08 02:08:38 +0900

    spring batch 세팅

--- seminarhub-batch/src/main/java/com/example/demo/DemoApplication.java
+++ seminarhub-batch/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.apress.batch;
+package com.example.demo;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableBatchProcessing
 @SpringBootApplication
-public class SeminarHubBatchApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeminarHubBatchApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- seminarhub-batch/src/test/java/com/example/demo/DemoApplicationTests.java
+++ seminarhub-batch/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.apress.batch.chapter10;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SeminarhubBatchApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
